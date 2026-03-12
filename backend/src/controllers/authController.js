815946a9d2275ccd90534702839f29f8b7bcc237
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-12 08:41:55 +0530

    refact : restructured a registerUser

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,59 +1,34 @@
-import User from "../models/User.js";
-import bcrypt from "bcryptjs";
+import { asyncHandler } from "../core/asyncHandler";
+import { HttpError } from "../core/httpException";
+import User from "../config/db.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-export const register = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
+export const register = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new HttpError(400, "All Fields are Required");
   }
-};
 
-export const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const existUser = await User.findOne({ email });
 
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
-
-    res.json({
-      token,
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
+  if (existUser) {
+    throw new HttpError(400, "User Already Exists");
   }
-};
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    name,
+    email,
+    password: hashPassword,
+  });
+
+  res.status(201).json({
+    success: true,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    },
+  });
+});
