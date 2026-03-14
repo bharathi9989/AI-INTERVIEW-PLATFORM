3e96f1f4d7105410bbbae1e13008dd05a2f5a8e7
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-14 20:55:30 +0530

    refact : restructure a controller and cetralize the service and added a controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,45 +1,10 @@
+import { loginService } from "../services/authService.js";
 import { asyncHandler } from "../core/asyncHandler.js";
-import { HttpError } from "../core/httpException.js";
-import User from "../config/db.js";
 
-import bcrypt from "bcryptjs";
-
-export const register = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    throw new HttpError(400, "All Fields are Required");
-  }
-
-  const existUser = await User.findOne({ email });
-
-  if (existUser) {
-    throw new HttpError(400, "User Already Exists");
-  }
-
-  const hashPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({
-    name,
-    email,
-    password: hashPassword,
-  });
-
-  res.status(201).json({
-    success: true,
-    user: {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-    },
-  });
-});
-
-export const login = asyncHandler(async (req, res) => { 
-
+export const login = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  if (!email || !password) { 
-    throw new HttpError(400, "All Fields are Required");
-  }
+  const result = await loginService(email, password);
 
-  const user = User.findOne({email})
-})
+  res.json(result);
+});
