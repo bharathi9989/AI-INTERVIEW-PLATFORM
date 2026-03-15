e43331e9555dc9c4b2236cdcf8e3a15efd518965
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-15 15:48:48 +0530

    fix : fixed port is working successfully

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,9 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 export const ENV = {
   DATABASE_URI: process.env.DATABASE_URI,
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 2026,
   JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
 };
