e3fa6453b78f252c472157e832a1f55c7a012503
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-17 22:44:21 +0530

    feat : added resume controller in resumeController

--- backend/src/controllers/resumeController.js
+++ backend/src/controllers/resumeController.js
@@ -1,1 +1,24 @@
 // Resume controller
+
+import { asyncHandler } from "../core/asyncHandler.js";
+import { processResumeService } from "../services/resumeService.js";
+
+export const uploadResume = asyncHandler(async (req, res) => {
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).json({
+      message: "No file uploaded",
+    });
+  }
+
+  const result = await processResumeService({
+    userId: req.user.id,
+    file,
+  });
+
+  res.status(201).json({
+    success: true,
+    data: result,
+  });
+});
