6480cc0aff3dd460f5d473644b37baf7505a428a
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-19 11:27:53 +0530

    feat : added a comment for how to access ENV variable and purpose

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// ENV file because security purpose its using a best way and avoid dublication just import ENV and invoke the method like object 
+
 export const ENV = {
   DATABASE_URI: process.env.DATABASE_URI,
   PORT: process.env.PORT || 2026,
