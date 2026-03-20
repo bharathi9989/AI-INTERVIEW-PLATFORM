aa9c6c958c0b327d9bc8b3da37e2dbed3a46164d
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-20 22:42:13 +0530

    feat : added a interview control

---
+++ backend/src/controllers/interviewController.js
@@ -0,0 +1,19 @@
+import { asyncHandler } from "../core/asyncHandler.js";
+import { evaluateAnswer } from "../services/aiService.js";
+
+export const evaluate = asyncHandler(async (req, res) => {
+  const { question, answer } = req.body;
+
+  if (!question || !answer) {
+    return res.status(400).json({
+      message: "Question and answer required",
+    });
+  }
+
+  const result = await evaluateAnswer(question, answer);
+
+  res.json({
+    success: true,
+    data: result,
+  });
+});
