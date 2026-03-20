125ea0b29117b5f20af7a64d816f54ae2e5c42ec
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-20 23:21:00 +0530

    refact  ; restructure and added comments proberly

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,25 @@ import { ENV } from "./src/config/env.js";
 import resumeRoutes from "./src/routes/resumeRoutes.js";
 import interviewRoutes from "./src/routes/interviewRoutes.js";
 
-
+// create server
 const app = express();
-
+// connected to DB
 connectDB();
 
+// client server communication purpose
 app.use(cors());
+
+// parse data in json type
 app.use(express.json());
 
+// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/resume", resumeRoutes);
 
 
 app.use("/api/interview", interviewRoutes);
 
+// listen the server in port
 app.listen(ENV.PORT, () => {
   console.log(`Server running on port ${ENV.PORT}`);
   console.log("connected to DataBase");
