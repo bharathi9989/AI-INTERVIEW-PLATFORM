147c420316288236ac37209629ff38d5a16a50e8
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-20 23:17:58 +0530

    feat : routes added to sever side for interview logic

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/authRoutes.js";
 import { ENV } from "./src/config/env.js";
 import resumeRoutes from "./src/routes/resumeRoutes.js";
+import interviewRoutes from "./src/routes/interviewRoutes.js";
 
 
 const app = express();
@@ -16,6 +17,9 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/resume", resumeRoutes);
+
+
+app.use("/api/interview", interviewRoutes);
 
 app.listen(ENV.PORT, () => {
   console.log(`Server running on port ${ENV.PORT}`);
