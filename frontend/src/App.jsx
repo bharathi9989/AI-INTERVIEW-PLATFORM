66c74a87960dc26b8e497049f0939a93a99dd18a
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-21 22:55:10 +0530

    feat : added routes page and written a sample logics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Upload from "./pages/Upload";
+import Interview from "./pages/Interview";
 
 function App() {
   return (
-    <div className='text-red-500'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/interview" element={<Interview />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
