cf06b690de06118f84ab85fcb3592d4fe6e8380a
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-21 22:56:17 +0530

    feat : added a login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useState, useContext } from "react";
+import API from "../api/axios";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await API.post("/auth/login", {
+      email,
+      password,
+    });
+
+    login(res.data.data);
+
+    navigate("/dashboard");
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="h-screen flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="p-6 shadow-lg rounded-lg w-80">
+        <h2 className="text-xl mb-4">Login</h2>
 
-export default Login
+        <input
+          className="border p-2 w-full mb-3"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          className="border p-2 w-full mb-3"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="bg-blue-500 text-white p-2 w-full">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
