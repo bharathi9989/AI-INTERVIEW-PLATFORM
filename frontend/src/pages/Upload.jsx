30d5df17aa2c2f3342eac56cf5c9cb6d61ae7176
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-22 21:50:13 +0530

    feat : added and update the upload page and logics in client side

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import API from "../api/axios";
 
-function Upload() {
+const Upload = () => {
+  const [file, setFile] = useState(null);
+  const [questions, setQuestions] = useState([]);
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("resume", file);
+
+    try {
+      const res = await API.post("/resume/upload", formData);
+
+      setQuestions(res.data.data.questions);
+    } catch (error) {
+      console.log(error);
+      alert("Upload failed");
+    }
+  };
+
   return (
-    <div>Upload</div>
-  )
-}
+    <div className="p-10">
+      <h1 className="text-xl mb-4">Upload Resume</h1>
 
-export default Upload
+      <input
+        type="file"
+        onChange={(e) => setFile(e.target.files[0])}
+        className="mb-4"
+      />
+
+      <button
+        onClick={handleUpload}
+        className="bg-green-500 text-white px-4 py-2"
+      >
+        Upload
+      </button>
+
+      {/* Questions */}
+      <div className="mt-6">
+        {questions.map((q, index) => (
+          <div key={index} className="mb-3 p-3 border rounded">
+            <p>{q.question}</p>
+            <small>
+              {q.type} | {q.difficulty}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Upload;
