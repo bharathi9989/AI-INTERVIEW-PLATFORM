856cd3ecbd86f02dd496a7868a0b0cff05c986ec
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-22 21:43:45 +0530

    fix : fixed a axios file for api intergration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,18 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
-})
+  baseURL: "http://localhost:5000/api",
+});
 
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-// Attach Token automatically
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-API.interceptors.request.use((config) => { 
-    const token = localStorage.getItem("token")
-    if (token) { 
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config;
-})
+  return config;
+});
 
 export default API;
