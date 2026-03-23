0527621d342b01ae0fd6822f720259a717e3e48e
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-23 22:36:34 +0530

    feat : evaluateBatchController added in routes

--- backend/src/routes/interviewRoutes.js
+++ backend/src/routes/interviewRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { evaluate } from "../controllers/interviewController.js";
+import { evaluate, evaluateBatchController } from "../controllers/interviewController.js";
 
 const router = express.Router();
 
 router.post("/evaluate", authMiddleware, evaluate);
+router.post("/evaluate-batch", authMiddleware, evaluateBatchController);
 
 export default router;
