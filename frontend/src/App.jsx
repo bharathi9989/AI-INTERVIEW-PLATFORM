c60f9b81e059aa150a71b480baabf481e497280a
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-23 22:27:21 +0530

    refact : we did go to login page automatically

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,15 @@ import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Upload from "./pages/Upload.jsx";
+import { Navigate } from "react-router-dom";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/upload" element={<Upload />} />
