1a25a2551d85f05683d2a06ddab765a4efd80251
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-23 22:42:44 +0530

    feat : added a feature in register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useState } from "react";
+import API from "../api/axios";
+import { useNavigate } from "react-router-dom";
 
-function Register() {
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await API.post("/auth/register", {
+        name,
+        email,
+        password,
+      });
+
+      alert("Registration successful");
+
+      navigate("/"); // go to login
+    } catch (error) {
+      console.log(error);
+      alert(error.response?.data?.message || "Register failed");
+    }
+  };
+
   return (
-    <div>Register</div>
-  )
-}
+    <div className="h-screen flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="p-6 shadow-lg rounded-lg w-80">
+        <h2 className="text-xl mb-4 text-center">Register</h2>
 
-export default Register
+        <input
+          className="border p-2 w-full mb-3"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          className="border p-2 w-full mb-3"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          className="border p-2 w-full mb-3"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="bg-green-500 text-white p-2 w-full">Register</button>
+
+        <p className="text-sm mt-3 text-center">
+          Already have an account?{" "}
+          <span
+            className="text-blue-500 cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            Login
+          </span>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
