31eb74b9a914f4458b07d0091860ac7a99a0589a
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-24 22:13:06 +0530

    feat : added a analytic routes in backend

---
+++ backend/src/routes/analyticsRoutes.js
@@ -0,0 +1,11 @@
+// src/routes/analyticsRoutes.js
+
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+import { getAnalytics } from "../controllers/analyticsController.js";
+
+const router = express.Router();
+
+router.get("/", authMiddleware, getAnalytics);
+
+export default router;
