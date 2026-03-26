d881c3475d235cda93c2226b722341299a3181a8
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-26 12:19:07 +0530

    fix : try to upload resume its showes error

--- backend/src/routes/resumeRoutes.js
+++ backend/src/routes/resumeRoutes.js
@@ -2,9 +2,20 @@ import express from "express";
 import upload from "../utils/multer.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 import { uploadResume } from "../controllers/resumeController.js";
+import multer from "multer";
+
 
 const router = express.Router();
 
+
 router.post("/upload", authMiddleware, upload.single("resume"), uploadResume);
+
+const upload = multer({ dest: "uploads/" });
+
+router.post("/upload", upload.single("resume"), (req, res) => {
+  console.log(req.file); // debug
+  res.json({ message: "Uploaded successfully" });
+});
+
 
 export default router;
