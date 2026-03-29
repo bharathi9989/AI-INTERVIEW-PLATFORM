0686bf5fc310b7ec9f64cfeb7c4f83d8434cea3d
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-29 22:41:00 +0530

    feat : update the evaluate batch controller

--- backend/src/controllers/interviewController.js
+++ backend/src/controllers/interviewController.js
@@ -30,20 +30,14 @@ export const evaluate = asyncHandler(async (req, res) => {
 // src/controllers/interviewController.js
 
 export const evaluateBatchController = asyncHandler(async (req, res) => {
-
   const { qaList } = req.body;
 
-  if (!qaList || qaList.length === 0) {
-    return res.status(400).json({
-      message: "qaList required"
-    });
-  }
-
-  const result = await evaluateBatch(qaList);
+  const results = await evaluateBatch(qaList);
 
   res.json({
     success: true,
-    data: result
+    data: {
+      results,
+    },
   });
-
 });
