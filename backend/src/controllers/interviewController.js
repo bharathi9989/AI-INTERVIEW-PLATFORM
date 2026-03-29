4649699efa150bc960a73b2add84a151a6dabd42
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-29 22:38:58 +0530

    feat: added a evaluate batch and sample mark is added a interview controller

--- backend/src/controllers/interviewController.js
+++ backend/src/controllers/interviewController.js
@@ -3,6 +3,13 @@
 import { asyncHandler } from "../core/asyncHandler.js";
 import { evaluateAnswer } from "../services/aiService.js";
 
+const evaluateBatch = async (qaList) => {
+  return qaList.map((item) => ({
+    score: 7, // dummy score
+    feedback: "Good answer, improve clarity",
+  }));
+};
+
 export const evaluate = asyncHandler(async (req, res) => {
   const { question, answer } = req.body;
 
