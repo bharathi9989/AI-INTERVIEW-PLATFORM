6b8a3ad4ad0ede07f3cfc5517df354ae1cb69799
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-29 22:50:13 +0530

    feat : to created a question in service

--- backend/src/services/resumeService.js
+++ backend/src/services/resumeService.js
@@ -11,3 +11,23 @@ export const processResumeService = async (filePath) => {
 
   return data.text;
 };
+
+export const generateQuestions = (text) => {
+  const questions = [];
+
+  if (text.toLowerCase().includes("javascript")) {
+    questions.push({ question: "What is closure in JavaScript?" });
+  }
+
+  if (text.toLowerCase().includes("node")) {
+    questions.push({ question: "Explain event loop in Node.js" });
+  }
+
+  if (text.toLowerCase().includes("react")) {
+    questions.push({ question: "What is useEffect?" });
+  }
+
+  questions.push({ question: "Tell me about yourself" });
+
+  return questions;
+};
