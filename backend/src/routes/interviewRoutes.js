d69871be0f98451f516d4c763fa7d7a6ce895676
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-31 15:50:32 +0530

    feat : added a routes in interview Routes

--- backend/src/routes/interviewRoutes.js
+++ backend/src/routes/interviewRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
 import { evaluate, evaluateBatchController } from "../controllers/interviewController.js";
+import { generateAnswer } from "../controllers/aicontroller.js";
 
 const router = express.Router();
 
 router.post("/evaluate", authMiddleware, evaluate);
 router.post("/evaluate-batch", authMiddleware, evaluateBatchController);
+router.post("/ai/generate", generateAnswer);
 
 export default router;
